87635ff6e0d425ed314c1a0272b1ad92f7eee4eb
yasingul <yasngl46@gmail.com>
2021-11-08 22:35:14 +0300

    3. Gün

---
+++ src/mapFilterReduce.js
@@ -0,0 +1,8 @@
+let cart = [
+    {id:1, productName:"Telefon", quantity:3, unitPrice:4500},
+    {id:2, productName:"Bardak", quantity:2, unitPrice:5},
+    {id:3, productName:"Kalem", quantity:1, unitPrice:45},
+    {id:4, productName:"Şarj Aleti", quantity:2, unitPrice:50},
+    {id:5, productName:"Kitap", quantity:3, unitPrice:35},
+    {id:6, productName:"Pot", quantity:5, unitPrice:135},
+]
